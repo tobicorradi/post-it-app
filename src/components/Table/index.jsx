3df8db6be8d57995f115201d2936534935e9ec4c
tobicorradi <corraditobias@gmail.com>
2021-11-10 22:25:14 -0300

    Modal added

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from '../Button'
+import Modal from 'react-modal'
 const Table = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+    const handleButton = () => {
+        setModalIsOpen(!modalIsOpen)
+    }
     return (
         <section>
             You have no Post It notes created
-            <Button text={'Crear Post It'} />
+            <Button onClick={handleButton} text={'Crear Post It'} />
+            <Modal
+                isOpen={modalIsOpen}
+                ariaHideApp={false}>
+                <Button onClick={handleButton} text={'Cancel'} />
+            </Modal>
         </section>
     )
 }
