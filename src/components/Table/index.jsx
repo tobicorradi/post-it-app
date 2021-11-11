73f618776b3b82c747d03c22f1ce0f37d0abe7d3
tobicorradi <corraditobias@gmail.com>
2021-11-11 19:04:22 -0300

    Redux installed and more tasks added

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -3,7 +3,7 @@ import ModalContent from '../ModalContent'
 import PostIt from '../PostIt'
 import './Table.css'
 const Table = () => {
-   const postList = [{id: 1, text: 'This is a sample text', color: '#33333'}]
+   const postList = [{id: 1, text: 'This is a sample text', color: '#33333'}, {id: 2, text: 'This is a sample text', color: '#33333'}]
     return (
         <section className="table">
             <div className="table__inner">
