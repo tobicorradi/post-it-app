3ac83d001d7f3ac0e62335459081215914ab38fe
tobicorradi <corraditobias@gmail.com>
2021-11-10 23:49:02 -0300

    Class added depending whereas the user is dragging post it

--- src/components/PostIt/index.jsx
+++ src/components/PostIt/index.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Draggable from 'react-draggable'
 import './PostIt.css'
 const PostIt = ({text, color}) => {
+    const [isDragging, setIsDragging] = useState(false)
+    const handleDrag = () => {
+        setIsDragging(!isDragging)
+    }
     return (
-        <Draggable>
-            <div className="post-it">
+        <Draggable onStart={handleDrag} onStop={handleDrag}>
+            <div className={`post-it ${ isDragging ? 'dragging' : ''}`}>
                 <p>{text}</p>
             </div>
         </Draggable>
