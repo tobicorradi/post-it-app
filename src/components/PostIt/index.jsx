51b7e2f5517ce4f65def3e9f5a4527da4d3446d3
tobicorradi <corraditobias@gmail.com>
2021-11-10 23:25:20 -0300

    First post it added (including styling)

--- src/components/PostIt/index.jsx
+++ src/components/PostIt/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './PostIt.css'
 const PostIt = ({text, color}) => {
     return (
-        <div>
-            {text}
+        <div className="post-it">
+            <p>{text}</p>
         </div>
     )
 }
--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -3,12 +3,12 @@ import ModalContent from '../ModalContent'
 import PostIt from '../PostIt'
 import './Table.css'
 const Table = () => {
-   
+   const postList = [{id: 1, text: 'This is a sample text', color: '#33333'}]
     return (
         <section className="table">
             <div className="table__inner">
                <ModalContent />
-               <PostIt text={'test'}/>
+               {postList.map(post => <PostIt text={post.text}/>)}
             </div>
         </section>
     )
