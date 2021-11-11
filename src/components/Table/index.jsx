95850eafd257190f4954614633f5f0ecc55f3932
tobicorradi <corraditobias@gmail.com>
2021-11-10 22:37:01 -0300

    Modal content created

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -6,14 +6,19 @@ const Table = () => {
     const handleButton = () => {
         setModalIsOpen(!modalIsOpen)
     }
+    const colorOptions = ['#66e0ff', '#66e0ff', '#66e0ff', '#66e0ff']
     return (
         <section>
             You have no Post It notes created
             <Button onClick={handleButton} text={'Crear Post It'} />
-            <Modal
-                isOpen={modalIsOpen}
-                ariaHideApp={false}>
+            <Modal isOpen={modalIsOpen} ariaHideApp={false}>
+                <h2>Crea tu post it</h2>
+                <form>
+                    <textarea placeholder="Terminar feature..."></textarea>
+                </form>
+                {colorOptions.map(color => <div>{color}</div>)}
                 <Button onClick={handleButton} text={'Cancel'} />
+                <Button onClick={handleButton} text={'Guardar'} />
             </Modal>
         </section>
     )
