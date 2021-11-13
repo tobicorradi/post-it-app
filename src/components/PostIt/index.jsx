fd7b09e737baf3dd5721193b6fac0c3b96ad35d5
tobicorradi <corraditobias@gmail.com>
2021-11-13 09:55:50 -0300

    Active state added to post it component

--- src/components/PostIt/index.jsx
+++ src/components/PostIt/index.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
 import Draggable from 'react-draggable'
+import {ReactComponent as ThreeDots} from './../../assets/icons/dots.svg'
 import './PostIt.css'
 const PostIt = ({text, color}) => {
+    const [active, setActive] = useState(false)
     const [isDragging, setIsDragging] = useState(false)
     const handleDrag = () => {
         setIsDragging(!isDragging)
     }
     return (
         <Draggable onStart={handleDrag} onStop={handleDrag}>
-            <div style={{maxWidth: 176}}>     
+            <div onClick={() => setActive(true)} style={{maxWidth: 176}}>     
+                {active ? <ThreeDots /> : null}
                 <div style={{transform: 'rotate(1deg)', background: color}} className={`post-it ${ isDragging ? 'dragging' : ''}`}>
                     <p style={{transform: 'rotate(-1deg)'}}>{text}</p>
                 </div>
